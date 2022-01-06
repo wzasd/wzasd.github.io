2da328abfcce9741cb8927e5d5912d67c462bab0
jeffrey <jayw2016@outlook.com>
2022-01-06 15:42:01 +0800

    feat:修改忽略文件夹

--- deploy.sh
+++ deploy.sh
@@ -28,11 +28,11 @@ doCommit() {
         echo $line >> $url_txt
     done
 
-    # echo "pushing urls to baidu"
+    echo "pushing urls to baidu"
 
-    # curl -H 'Content-Type:text/plain' --data-binary "@$url_txt" "http://data.zz.baidu.com/urls?site=https://blog.yorek.xyz&token=XAt9QHfqMXWRRMoU"
+    curl -H 'Content-Type:text/plain' --data-binary "@$url_txt" "http://data.zz.baidu.com/urls?site=https://wzasd.github.io&token=gv0ua2vUtIH0z7bt"
 
-    # echo "push completed."
+    echo "push completed."
 
     rm -f $url_txt
 
